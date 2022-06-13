dc567bfcacf56b74ae061d3b9376be3f6102d999
Satyam Teotia <satyamteotia1998@gmail.com>
2022-06-13 21:49:31 +0530

    new commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
+const shape = require("Shape");
 
 // declare class
+class Circle extends shape {
+    drawShape = function() {
+
+    }
+    calculateArea = function(side) {
+        return (Math.PI * (side * side));
+    }
+}
 
 
 // export class using module.exports
+module.exports(Circle);
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
+class Shape {
+    constructor() {
+        let color = "red";
+        drawShape = function() {
 
+        }
+        calculateArea = function(side) {
+            return side*side;
+        }
+    }
+}
 // export class using module.exports
+module.exports(Shape);
